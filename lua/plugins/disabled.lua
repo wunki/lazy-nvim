df140e09bac91665fd5bd7a8e90d6089af016062
Petar Radosevic <petar@petar.dev>
2024-09-16 09:12:51 +0200

    saner defaults

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -3,4 +3,5 @@ return {
   { "akinsho/bufferline.nvim", enabled = false },
   { "nvimdev/dashboard-nvim", enabled = false },
   { "folke/noice.nvim", enabled = false },
+  { "lukas-reineke/indent-blankline.nvim", opts = { enabled = false } },
 }

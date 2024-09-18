5219df35f402efcd63cdce793e8156916b5f6788
Petar Radosevic <petar@petar.dev>
2024-09-18 12:56:52 +0200

    fix anoying notifications

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -11,3 +11,19 @@ opt.smarttab = true
 opt.softtabstop = 4
 opt.tabstop = 4
 opt.list = false
+
+-- This disables the notifications when you try to get information
+-- and one of the LSPs has no information available.
+vim.lsp.handlers["textDocument/hover"] = function(_, result, ctx, config)
+  config = config or {}
+  config.focus_id = ctx.method
+  if not (result and result.contents) then
+    return
+  end
+  local markdown_lines = vim.lsp.util.convert_input_to_markdown_lines(result.contents)
+  markdown_lines = vim.lsp.util.trim_empty_lines(markdown_lines)
+  if vim.tbl_isempty(markdown_lines) then
+    return
+  end
+  return vim.lsp.util.open_floating_preview(markdown_lines, "markdown", config)
+end

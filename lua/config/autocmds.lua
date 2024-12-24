7f98cfb4609a9020b9344ca412959f43f49d5d55
Petar Radosevic <petar@petar.dev>
2024-12-24 22:39:12 +0100

    feat(autocmd): add gitcommit file type auto-start insert mode

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,14 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "gitcommit",
+  callback = function()
+    vim.cmd("startinsert")
+    -- Jump to the end of the file, then to the end of the line
+    vim.cmd("normal! G$")
+    -- Now start insert mode
+    vim.cmd("startinsert")
+  end,
+})

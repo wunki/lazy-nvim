754272482fb457fd7c8ca5cefc8b9621a97dd047
Petar Radosevic <petar@petar.dev>
2024-12-25 07:25:05 +0100

    autocommand to start git commit in insert mode

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,9 +6,5 @@ vim.api.nvim_create_autocmd("FileType", {
   pattern = "gitcommit",
   callback = function()
     vim.cmd("startinsert")
-    -- Jump to the end of the file, then to the end of the line
-    vim.cmd("normal! G$")
-    -- Now start insert mode
-    vim.cmd("startinsert")
   end,
 })

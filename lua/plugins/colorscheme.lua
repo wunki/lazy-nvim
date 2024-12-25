556d3788aecc217d02d6f028bda10ce0b85f72c6
Petar Radosevic <petar@petar.dev>
2024-12-25 07:25:39 +0100

    Material gruvbox theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -17,11 +17,32 @@ return {
     },
   },
   {
+    "catppuccin/nvim",
+    enabled = false,
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme("catpuccin")
+    end,
+  },
+  {
     "AlexvZyl/nordic.nvim",
+    enabled = false,
     lazy = false,
     priority = 1000,
     config = function()
       require("nordic").load()
+    end,
+  },
+  {
+    "sainnhe/gruvbox-material",
+    enabled = true,
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.gruvbox_material_background = "hard"
+      vim.g.gruvbox_material_enable_italic = true
+      vim.cmd.colorscheme("gruvbox-material")
     end,
   },
 }

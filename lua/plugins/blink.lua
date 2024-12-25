1ddc5cf120d4a827c857a8c91f174a926e203b98
Petar Radosevic <petar@petar.dev>
2024-12-25 07:24:42 +0100

    Configuration for blink autocomplete

---
+++ lua/plugins/blink.lua
@@ -0,0 +1,13 @@
+return {
+  "saghen/blink.cmp",
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
+  opts = {
+    -- keymap = { preset = "default" },
+    completion = {
+      menu = {
+        auto_show = true,
+      },
+    },
+  },
+}
